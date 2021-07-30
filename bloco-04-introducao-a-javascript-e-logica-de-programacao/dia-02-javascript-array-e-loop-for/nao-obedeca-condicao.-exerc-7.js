1a77e7b1d85f3eb49e8d14c8b2b01830c46feaf3
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-07-30 19:12:02 -0300

    Variável não obedece condição: Imprimir valor menor que 16 - Parte 7

---
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/nao-obedeca-condicao.-exerc-7.js
@@ -0,0 +1,16 @@
+//exercise.js
+//7. Caso a varíavel obedeça a condição: Imprimir a varíavel;
+
+//exercise.js
+let fruits = [3, 4, 10, 1, 12];
+let sum = 0;
+
+for (let index = 0; index < fruits.length; index += 1) {
+  sum += fruits[index];
+}
+
+if (sum > 15) {
+  console.log(sum);
+} else {
+  console.log('valor menor que 16');
+}
