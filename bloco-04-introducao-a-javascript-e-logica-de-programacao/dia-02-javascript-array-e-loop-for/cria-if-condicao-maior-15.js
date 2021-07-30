3e3a8797d94a8184cbdd82d336eff245af927a10
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-07-30 14:44:16 -0300

    Adiciona Condiçnao Número maior que 15 - Parte 5

---
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/cria-if-condicao-maior-15.js
@@ -0,0 +1,15 @@
+//exercise.js
+//5. Criar um if com a condição da váriavel ser maior que 15;
+
+let fruits = [3, 4, 10, 1, 12];
+let sum = 0;
+
+for (let index = 0; index < fruits.length; index += 1) {
+  sum += fruits[index];
+}
+
+if (sum > 15) {
+
+} else {
+    
+}
