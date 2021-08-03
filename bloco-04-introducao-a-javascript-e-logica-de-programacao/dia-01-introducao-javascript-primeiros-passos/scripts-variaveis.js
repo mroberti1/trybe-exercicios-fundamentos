400e294599091ec8f33039d0c7369cd3c0f4bb68
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-08-03 17:23:01 -0300

    Altera nome de arquivo

---
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript-primeiros-passos/scripts-variaveis.js
@@ -0,0 +1,11 @@
+//Constante não muda
+
+const myName = "Mauricio";
+const birthCity = "São Paulo";
+const birthYear = "1980";
+
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
