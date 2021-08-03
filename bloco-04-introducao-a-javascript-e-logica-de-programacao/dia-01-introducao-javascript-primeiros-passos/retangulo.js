9219169f790ef5f2b480a046b86ad72857a8ba9f
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-08-03 18:53:40 -0300

    Cálculo de um retângulo - Perímetro e Área

---
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript-primeiros-passos/retangulo.js
@@ -0,0 +1,20 @@
+
+//AREA: Equivale ao tamanho da superfície
+
+const base = 5;
+let height = 8;
+const area = base * height;
+const perimeter = (base + height + area);
+console.log(area);
+console.log(perimeter);
+
+//Perímetro: É o resultado da somas dos seus lados 
+
+// const base = 5;
+// const height = 8;
+// const area = base * height;
+// console.log(area);
+
+// //Perimeter = base + base + height + height
+// const perimeter = 5 + 5 + 8 + 8;
+// console.log(perimeter);
