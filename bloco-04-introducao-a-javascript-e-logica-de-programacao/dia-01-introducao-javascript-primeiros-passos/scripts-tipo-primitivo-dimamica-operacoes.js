8f95dbf84e889293892e32cc50c7713491d1d8e7
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-08-03 17:43:04 -0300

    Tipos Primitivos, Tipagem Dinâmica e Operações Aritméticas

---
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript-primeiros-passos/scripts-tipo-primitivo-dimamica-operacoes.js
@@ -0,0 +1,30 @@
+//Tipos Primitivos, Tipagem Dinâmica e Operações Aritméticas
+
+//Tipos Primitivos
+// let movie = "Avengers"; //Tipo String Literal
+// let scrore = 10.89; //Tipo Number Literal
+// let isValid = true; // Tipo Booleano
+// let name; Tipo; //Indefinido Undefined
+// let color = null; //Pesso muda a Cor do site
+
+//Tipagem Dinâmica
+
+//Quando coloca o JS no HTML
+//O tipo de variável pode mudar, salvo a const
+
+//Operações Aritméticas
+
+let salary = 3500;
+
+console.log(salary + salary);
+console.log(salary - salary);
+console.log(salary * salary);
+console.log(salary / salary);
+console.log(salary + salary);
+console.log(3 ** 3); //exponenciação
+
+salary++;
+console.log(salary) //acrescenta + 1
+
+salary--;
+console.log(salary) //acrescenta - 1
