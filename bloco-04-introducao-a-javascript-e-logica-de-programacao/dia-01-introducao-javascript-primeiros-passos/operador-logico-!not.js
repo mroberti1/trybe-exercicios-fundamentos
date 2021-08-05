89d10f1da17cd7d98c5e6cb41e3a2cd8aa2bf3c8
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-08-05 19:47:39 -0300

    Adiciona Operador Lógico ! not

---
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript-primeiros-passos/operador-logico-!not.js
@@ -0,0 +1,25 @@
+// Operador de Negação ! Not
+console.log ((2 + 2) === 4); //true
+
+console.log (!(2 + 2) === 4); // false pelo operador ! antes da declaração. Inverte o valor Booleano
+
+console.log();
+
+//string
+const squirtle = "melhor pokemon inicial";
+console.log(!squirtle); //false
+
+console.log();
+
+console.log(!42); //false
+console.log(!0); //true
+// O número 0 tem o valor "falsy" no javascript. Logo, seu oposto é true.
+
+console.log();
+
+//Valores Nulos
+console.log(!null); // true
+
+console.log();
+
+console.log(!undefined); // true
