c6e26ef178e8c66c102676a6ceb22b89c6c11f9d
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-08-05 00:23:49 -0300

    Adicionando Estruturas Condicionais  if/else

---
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript-primeiros-passos/estruturas-condicionais-if-else.js
@@ -0,0 +1,11 @@
+const studentGrade = 59;
+
+if(studentGrade >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!");
+
+} else if(studentGrade < 80 || studentGrade >= 60) {
+    console.log("Você está em nossa lista de espera!");
+
+} else if (studentGrade < 60) {
+    console.log("Você foi reprovada(o)!");
+}
